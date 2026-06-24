05808600bd3f3af449989dfdc173f55c007b82cd
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-06-24 01:33:25 -0400

    Added Cutscene 1->2 and all music

--- Cutscene12/cutscene12.gd
+++ Cutscene12/cutscene12.gd
@@ -1,12 +1,20 @@
-extends Node2D
+extends AnimatedSprite2D
 
 var void_world = "res://World2/world2.tscn"
 
+var current_scene = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(5.0).timeout
-	switch_scene.switch_scene(void_world)
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	if Input.is_action_just_pressed("next_scene"):
+		if current_scene < 9:
+			current_scene += 1
+		else:
+			switch_scene.switch_scene(void_world)
+
+		self.set_frame_and_progress(current_scene, 0.0)
